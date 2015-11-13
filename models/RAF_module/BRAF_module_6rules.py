2612c31f45b33cf00f2db1e694179cb24c96328f
subkar <sub.kartik@gmail.com>
2015-11-13 14:43:40 -0500

    changed function name from observables to declare_obsrervables

--- models/RAF_module/BRAF_module_6rules.py
+++ models/RAF_module/BRAF_module_6rules.py
@@ -1,4 +1,5 @@
-""" Detailed mehanistic model of BRAF based on Neal Rossen paper. """
+""" Detailed mehanistic model of BRAF based on Neal Rossen paper.
+This module has fewer rules """
 from pysb import *
 from pysb.util import alias_model_components
 
@@ -12,36 +13,38 @@ def monomers():
     # --------
     Parameter('BRAF_0', 1e3)
     Parameter('KRAS_0', 100)
-    Parameter('Vem_0', 1000)
+    Parameter('Vem_0', 0)
 
     alias_model_components()
 
     # Initial conditions
     # ------------------
-    Initial(BRAF(d=None, ras=None, erk=None, vem=None), BRAF_0)
+    Initial(BRAF(d=None, ras=None, vem=None, erk=None), BRAF_0)
     Initial(KRAS(raf=None), KRAS_0)
     Initial(Vem(raf=None), Vem_0)
 
 
 def BRAF_dynamics():
+
     # Parameters
     # -----------
-    Parameter('kaf', 0.0001)
+    Parameter('kaf', 1e-4)  # 1e-5
     Parameter('kar', 1)
     Parameter('kbf', 1)
     Parameter('kbr', 1e-9)
-    Parameter('kcf', 1)
+    Parameter('kcf',  1)
     Parameter('kcr', 0.0001)
     Parameter('kdf', 1)
     Parameter('kdr', 0.1)
     Parameter('kef', 1)
     Parameter('ker', 0.1)
-    Parameter('kff', 0.001)
-    Parameter('kfr', 1)
+    Parameter('kff',  0.001)
+    Parameter('kfr', 1)  # 0.1
     Parameter('kgf', 1e-7)
     Parameter('kgr', 1)
-    Parameter('khf', 1)  # 100)
-    Parameter('khr', 1)  # 1)
+    Parameter('khf', 1)
+    Parameter('khr', 1)
+    Parameter('koff', 1)
 
     alias_model_components()
 
@@ -49,36 +52,19 @@ def BRAF_dynamics():
     # -----
 
     # BRAF dimerization
-    Rule('BRAF_dimerization',
-         BRAF(d=None, ras=None) + BRAF(d=None, ras=None, vem=None) <>
-         BRAF(d=1, ras=None) % BRAF(d=1, ras=None, vem=None), kaf, kar)
+    Rule('BRAF_binds_BRAF',
+         BRAF(d=None) + BRAF(d=None, ras=None) <>
+         BRAF(d=1) % BRAF(d=1, ras=None), kaf, kar)
 
     # KRAS binding BRAF monomers
-    Rule('KRAS_binding_BRAF_monomers',
-         BRAF(ras=None, d=None) + KRAS(raf=None) <>
-         BRAF(ras=1, d=None) % KRAS(raf=1), kdf, kdr)
-
-    # KRAS binding BRAF dimers
-    Rule('KRAS_binding_BRAF_dimers',
-         BRAF(ras=None, d=1) % BRAF(ras=None, d=1) +
-         KRAS(raf=None) + KRAS(raf=None) <>
-         BRAF(ras=2, d=1) % BRAF(ras=3, d=1) %
-         KRAS(raf=2) % KRAS(raf=3), kbf, kbr)
+    Rule('KRAS_binds_BRAF',
+         BRAF(ras=None) + KRAS(raf=None) <>
+         BRAF(ras=1) % KRAS(raf=1), kdf, kdr)
 
     # KRAS:BRAF dimerization
-    Rule('KRASBRAF_dimerization',
-         BRAF(d=None, ras=ANY) + BRAF(d=None, ras=ANY, vem=None) <>
-         BRAF(d=1, ras=ANY) % BRAF(d=1, ras=ANY, vem=None), kcf, kcr)
-
-    # BRAF:Vem dimerization to give 2(BRAF:Vem) g = a * f
-    Rule('BRAF_Vem_dimerization',
-         BRAF(d=None, ras=None, vem=ANY) + BRAF(d=None, ras=None, vem=ANY) <>
-         BRAF(d=1, ras=None, vem=ANY) % BRAF(d=1, ras=None, vem=ANY), kgf, kgr)
-
-    # KRAS:BRAF:Vem dimerization to give 2( KRAS:BRAF:Vem) h = c * a
-    Rule('KRAS_BRAF_Vem_dimerization',
-         BRAF(d=None, ras=ANY, vem=ANY) + BRAF(d=None, ras=ANY, vem=ANY) <>
-         BRAF(d=1, ras=ANY, vem=ANY) % BRAF(d=1, ras=ANY, vem=ANY), khf, khr)
+    Rule('KRAS_BRAF_dimerization',
+         BRAF(d=None, ras=ANY) + BRAF(d=None, ras=ANY) <>
+         BRAF(d=1, ras=ANY) % BRAF(d=1, ras=ANY), kcf, kcr)
 
     # 1st Vemurafenib binds
     Rule('First_binding_Vemurafenib',
@@ -100,10 +86,10 @@ def declare_observables():
     # Observables
     # ----------
     Observable('BRAF_WT_active',
-               BRAF(d=ANY, vem=None)) 
+               BRAF(d=ANY, vem=None))
 
     Observable('BRAF_V600E_active',
-               BRAF(vem=None)) 
+               BRAF(vem=None))
 
 # if __name__ == '__main__':
 
