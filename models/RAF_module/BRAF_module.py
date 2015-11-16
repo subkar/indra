98e89c33ead470b696d81149d92defcab301bae8
subkar <sub.kartik@gmail.com>
2015-11-16 17:39:27 -0500

    1. Initial amounts of BRAF and KRAS were adjusted to match estimates (copies/cell) shown in Mario's manuscript 2. Biochemical parameters were adjusted accordingly to recreate simulation results

--- models/RAF_module/BRAF_module.py
+++ models/RAF_module/BRAF_module.py
@@ -10,9 +10,9 @@ def monomers():
 
     # IC values
     # --------
-    Parameter('BRAF_0', 1e3)
-    Parameter('KRAS_0', 100)
-    Parameter('Vem_0', 1000)
+    Parameter('BRAF_0', 1e5)
+    Parameter('KRAS_0', 2e5)
+    Parameter('Vem_0', 1e5)
 
     alias_model_components()
 
@@ -26,21 +26,21 @@ def monomers():
 def BRAF_dynamics():
     # Parameters
     # -----------
-    Parameter('kaf', 0.0001)
+    Parameter('kaf', 1e-6)  # 0.0001)
     Parameter('kar', 1)
     Parameter('kbf', 1)
-    Parameter('kbr', 1e-9)
+    Parameter('kbr', 1e-11)
     Parameter('kcf', 1)
     Parameter('kcr', 0.0001)
     Parameter('kdf', 1)
     Parameter('kdr', 0.1)
-    Parameter('kef', 1)
+    Parameter('kef', 1e-2)
     Parameter('ker', 0.1)
-    Parameter('kff', 0.001)
+    Parameter('kff', 1e-5)
     Parameter('kfr', 1)
-    Parameter('kgf', 1e-7)
+    Parameter('kgf', 1e-11)
     Parameter('kgr', 1)
-    Parameter('khf', 1)  # 100)
+    Parameter('khf', 1e-2)  # 100)
     Parameter('khr', 1)  # 1)
 
     alias_model_components()
