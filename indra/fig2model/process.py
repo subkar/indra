2c5ad5a2ed104e432a34caa9d770be34691ec479
subkar <sub.kartik@gmail.com>
2016-05-04 16:16:59 -0400

    replace panel_ids with links

--- indra/fig2model/process.py
+++ indra/fig2model/process.py
@@ -2,7 +2,8 @@
 def get_pmids(json):
     papers = {}
     for _, paper in enumerate(json['results']['direct']):
-        paper_key = 'PMID' + paper['pmid']
+        if paper['pmid'] != None:
+            paper_key = 'PMID' + paper['pmid']
         papers[paper_key] = get_hypos(paper)
     return papers
 
@@ -17,16 +18,11 @@ def get_hypos(paper):
 
 def get_panel_ids(hypo):
     ids = []
+    fig_base_url = 'http://sourcedata.vital-it.ch/public/#/panel/'
     for _, panel in enumerate(hypo['panels']):
-        ids.append(panel['panel_id'])
+        ids.append(fig_base_url + panel['panel_id'])
     return ids
 
-# class links:
-#     def __init__(self, hypo):
-#         ids = []
-#         for _, panel in enumerate(hypo['panels']):
-#             ids.append(panel['panel_id'])
-#         self.panels = ids
 
 
 
