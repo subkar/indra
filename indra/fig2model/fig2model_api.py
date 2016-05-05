8db06c38710d0f2dfa12c4772b1db5133f51e0c8
subkar <sub.kartik@gmail.com>
2016-05-05 16:37:58 -0400

    refactored code to enable hypotheses collection only for monomer pairs in the model

--- indra/fig2model/fig2model_api.py
+++ indra/fig2model/fig2model_api.py
@@ -1,17 +1,19 @@
 import sourcedata_client as sdc
 from process import Publication, Hypothesis
 
-def process_query(query, save_json_name = 'sd_output.json'):
+def process_query(query, mlist=[], save_json_name = 'sd_output.json'):
     dict = sdc.get_json(query)
     sdc.save_json(dict, save_json_name)
-    return process_json(dict)
+    return process_json(dict, mlist)
 
 
-def process_json(dict):
+def process_json(dict, mlist):
     papers = []
     for paper in dict['results']['direct']:
         if paper['pmid'] != None:
-            papers.append(Publication(paper))
+            p = Publication(paper)
+            p.hypotheses(paper, mlist)
+            papers.append(p)            
     return papers
 
 
--- indra/fig2model/process.py
+++ indra/fig2model/process.py
@@ -1,19 +1,34 @@
 class Publication(object):
     def __init__(self, paper):
         self.pmid = paper['pmid']
-        self.hypotheses = [Hypothesis(h) for h in paper['hypos']]
+        # self.hypotheses = [Hypothesis(h) for h in paper['hypos']]
+    
 
     def __str__(self):
         return ("Publication PMID:" + self.pmid)
 
+    
     def __repr__(self):
         return ("<pmid: %s, %d hypotheses at 0x%x>" %
                 (self.pmid, len(self.hypotheses), id(self)))
 
 
+    def hypotheses(self, paper, mlist):
+        if not mlist:
+            self.hypotheses = [Hypothesis(h) for h in paper['hypos']]
+        elif mlist:
+            slist = []
+            for h in paper['hypos']:
+                hy = Hypothesis(h)
+                if hy.assayed and hy.intervention in mlist:
+                    slist.append(hy)
+            self.hypotheses = slist
+
 class Hypothesis(object):
     def __init__(self, hypothesis):
         self.assertion = hypothesis['hypo']
+        self.assayed = hypothesis['displayRight'][0].lower()
+        self.intervention = hypothesis['displayLeft'][0].lower()
         ids = []
         fig_base_url = 'http://sourcedata.vital-it.ch/public/#/panel/'
         self.panels = [fig_base_url + panel['panel_id']
