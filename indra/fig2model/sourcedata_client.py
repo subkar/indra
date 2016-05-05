537f9f04026edbcc7e24713d12b17e60071db0e8
subkar <sub.kartik@gmail.com>
2016-05-05 13:31:26 -0400

    cosmetic change

--- indra/fig2model/sourcedata_client.py
+++ indra/fig2model/sourcedata_client.py
@@ -10,8 +10,7 @@ base_url = 'http://sourcedata.vital-it.ch/public/php/api/index.php'
 def get_json(search_term, type='generic', limit=20, motif='scale'):
     params = {'limit': limit, 'motif': motif}
     search_url = base_url + '/%s/' % type + '%s' % search_term
-    r = requests.get(search_url, params,
-                     auth=key)
+    r = requests.get(search_url, params, auth=key)
     r.raise_for_status()
     dict = r.json()
     return dict
