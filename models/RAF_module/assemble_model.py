a730c82d18bf9764d94dbc85209ffa83ce7f8494
Ben Gyori <ben.gyori@gmail.com>
2016-06-06 18:38:59 -0400

    Use diffs in assemble model and some other changes

--- models/RAF_module/assemble_model.py
+++ models/RAF_module/assemble_model.py
@@ -1,20 +1,54 @@
 import os
 import sys
-sys.path.append('../..')
-from indra.assemblers import PysbAssembler
-from indra import trips
-import pysb.export
 from pysb import *
+import pysb.export
+from indra import trips
+from indra.assemblers import PysbAssembler
 from run_model import run_model
 from enumerate_rules import enumerate_rules
 
 
+def apply_patch(original, patch):
+    orig_lines = [s.strip() for s in original.strip().split('\n')]
+    patch_lines = [s.strip() for s in patch.strip().split('\n')]
+    remove_lines = []
+    add_lines = []
+    for p in patch_lines:
+        if p.startswith('<'):
+            remove_lines.append(p[2:].strip())
+        elif p.startswith('>'):
+            add_lines.append(p[2:].strip())
+    new_lines = []
+    for o in orig_lines:
+        if o not in remove_lines:
+            new_lines.append(o)
+    new_lines += add_lines
+    new_txt = '\n'.join(new_lines)
+    return new_txt
+
 for i in range(4):
-    tp = trips.process_text(open('model_%s.txt' % (i+1)).read())
+    print 'Reading model %d' % (i+1)
+    model_name = 'model_%d' % (i+1)
+    # If model has already been read, just process the EKB XML
+    if os.path.exists(model_name + '.xml'):
+        tp = trips.process_xml(open(model_name + '.xml').read())
+    else:
+        # Start with the basic model
+        model_txt = open('model_1.txt').read()
+        # Apply patches one by one to get to the current model text
+        for j in range(1, i+1):
+            patch_txt = open('model_%d_from_%d.txt' % (j+1, j)).read()
+            model_txt = apply_patch(model_txt, patch_txt)
+        print model_txt
+        # Process model text and save result EKB XML
+        tp = trips.process_text(model_txt, model_name + '.xml')
+
+    # Assemble the PySB model
     pa = PysbAssembler()
     pa.add_statements(tp.statements)
     model = pa.make_model(policies='two_step')
-    
+
+    # Set initial conditions
     erk = model.monomers['MAPK1']
     obs = Observable('ERK_p', erk(phospho='p'))
     vem = model.monomers['VEMURAFENIB']
@@ -27,9 +61,8 @@ for i in range(4):
     braf = model.monomers['BRAF']
     model.parameters['BRAF_0'].value = 0
 
+    # Add mutated form of BRAF as initial condition
     sites_dict = {}
-
-
     for site in braf.sites:
         if site in braf.site_states:
             sites_dict[site] = braf.site_states[site][0]
@@ -39,6 +72,7 @@ for i in range(4):
     model.add_component(Parameter('BRAF_mut_0', 1e5))
     model.initial(braf(**sites_dict), model.parameters['BRAF_mut_0'])
 
+    # Set up model parameters
     model.parameters['kf_ee_bind_1'].value = 1
     model.parameters['kr_ee_bind_1'].value = 0.1
     model.parameters['kf_ee_bind_2'].value = 1
@@ -56,7 +90,7 @@ for i in range(4):
 
     model.parameters['kf_vb_bind_1'].value = 0.1
     model.parameters['kr_vb_bind_1'].value = 0.1
-    
+
     model.parameters['kf_bm_bind_1'].value = 1
     model.parameters['kr_bm_bind_1'].value = 0.1
     model.parameters['kc_bm_phos_1'].value = 3
@@ -82,7 +116,6 @@ for i in range(4):
     model.parameters['MAPK1_0'].value = 1e5
     model.parameters['DUSP6_0'].value = 1e3
     model.parameters['PPP2CA_0'].value = 1e5
-    
 
     # feedback_params = ['PHOSPHATASE_0',
     #                   'kf_ms_bind_1', 'kr_ms_bind_1', 'kc_ms_phos_1',
@@ -97,42 +130,43 @@ for i in range(4):
         model.parameters['kf_ls_bind_1'].value = 1
         model.parameters['kr_ls_bind_1'].value = 0.1
         model.parameters['kc_ls_dephos_1'].value = 1e-04
-        
 
     # braf_dimerization_params = ['kf_bb_bind_1', 'kr_bb_bind_1']
-    
     # if set(braf_dimerization_params) < set([p.name for p in model.parameters]):
     if i == 2:
         model.parameters['kf_bb_bind_1'].value = 1
         model.parameters['kr_bb_bind_1'].value = 1e-04
 
     if i == 3:
-        enumerate_rules(model, model.rules['BRAF_BRAF_bind'], ['map2k1', 'V600'])
-        enumerate_rules(model, model.rules['BRAF_BRAF_dissociate'], ['map2k1', 'V600'])
-        enumerate_rules(model, model.rules['VEMURAFENIB_BRAF_bind'], ['map2k1', 'nras', 'V600'])
-        enumerate_rules(model, model.rules['VEMURAFENIB_BRAF_dissociate'], ['map2k1', 'nras', 'V600'])
-        
+        enumerate_rules(model, model.rules['BRAF_BRAF_bind'],
+                        ['map2k1', 'V600'])
+        enumerate_rules(model, model.rules['BRAF_BRAF_dissociate'],
+                        ['map2k1', 'V600'])
+        enumerate_rules(model, model.rules['VEMURAFENIB_BRAF_bind'],
+                        ['map2k1', 'nras', 'V600'])
+        enumerate_rules(model, model.rules['VEMURAFENIB_BRAF_dissociate'],
+                        ['map2k1', 'nras', 'V600'])
+
         # model.parameters['kf_bb_bind_1_nA_vA_nA_vA'].value =  1e-02
         # model.parameters['kr_bb_bind_1_nA_vA_nA_vA'].value = 1
-        model.parameters['kf_bb_bind_1_nN_vN_nN_vN'].value = 1e-06 
-        model.parameters['kf_bb_bind_1_nN_vN_nN_vA'].value = 1e-03 
-        model.parameters['kf_bb_bind_1_nN_vN_nA_vN'].value = 1e-06 
-        model.parameters['kf_bb_bind_1_nN_vN_nA_vA'].value = 1e-06 
-        model.parameters['kf_bb_bind_1_nN_vA_nN_vA'].value = 1e-08 
-        model.parameters['kf_bb_bind_1_nN_vA_nA_vN'].value = 1e-06 
-        model.parameters['kf_bb_bind_1_nN_vA_nA_vA'].value = 1e-06 
+        model.parameters['kf_bb_bind_1_nN_vN_nN_vN'].value = 1e-06
+        model.parameters['kf_bb_bind_1_nN_vN_nN_vA'].value = 1e-03
+        model.parameters['kf_bb_bind_1_nN_vN_nA_vN'].value = 1e-06
+        model.parameters['kf_bb_bind_1_nN_vN_nA_vA'].value = 1e-06
+        model.parameters['kf_bb_bind_1_nN_vA_nN_vA'].value = 1e-08
+        model.parameters['kf_bb_bind_1_nN_vA_nA_vN'].value = 1e-06
+        model.parameters['kf_bb_bind_1_nN_vA_nA_vA'].value = 1e-06
         model.parameters['kf_bb_bind_1_nA_vN_nA_vN'].value = 1
-        model.parameters['kf_bb_bind_1_nA_vN_nA_vA'].value = 1 
-        model.parameters['kr_bb_bind_1_nN_vN_nN_vN'].value = 1 
-        model.parameters['kr_bb_bind_1_nN_vN_nN_vA'].value = 1 
+        model.parameters['kf_bb_bind_1_nA_vN_nA_vA'].value = 1
+        model.parameters['kr_bb_bind_1_nN_vN_nN_vN'].value = 1
+        model.parameters['kr_bb_bind_1_nN_vN_nN_vA'].value = 1
         model.parameters['kr_bb_bind_1_nN_vN_nA_vN'].value = 1e-06
         model.parameters['kr_bb_bind_1_nN_vN_nA_vA'].value = 1e-06
-        model.parameters['kr_bb_bind_1_nN_vA_nN_vA'].value = 1 
-        model.parameters['kr_bb_bind_1_nN_vA_nA_vN'].value =  1e-06 
-        model.parameters['kr_bb_bind_1_nN_vA_nA_vA'].value = 1e-06 
-        model.parameters['kr_bb_bind_1_nA_vN_nA_vN'].value = 1e-04 
-        model.parameters['kr_bb_bind_1_nA_vN_nA_vA'].value = 1e-04 
-
+        model.parameters['kr_bb_bind_1_nN_vA_nN_vA'].value = 1
+        model.parameters['kr_bb_bind_1_nN_vA_nA_vN'].value =  1e-06
+        model.parameters['kr_bb_bind_1_nN_vA_nA_vA'].value = 1e-06
+        model.parameters['kr_bb_bind_1_nA_vN_nA_vN'].value = 1e-04
+        model.parameters['kr_bb_bind_1_nA_vN_nA_vA'].value = 1e-04
 
         #model.parameters['kf_vb_bind_1_bA'].value =  1e-02
         #model.parameters['kr_vb_bind_1_bA'].value = 1e-01
@@ -144,13 +178,6 @@ for i in range(4):
         # The pysb file generated for model 4 has to be manually modified.
         # Refer model_4a.py
 
- 
+    pa.model = model
     pa.save_model('model_%d.py' % (i+1))
     run_model(model, 'model_%d_plot.png' % (i+1))
-
-
-
-
-
-
-    
