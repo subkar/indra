390c22f6ee4f96ef5f55d65995cfb698839df89d
Ben Gyori <ben.gyori@gmail.com>
2016-06-06 21:56:34 -0400

    Small changes in running model

--- models/RAF_module/run_model.py
+++ models/RAF_module/run_model.py
@@ -1,9 +1,8 @@
-def run_model(model, save_plot='model_plot.png'):
-    
-    from pysb.integrate import Solver
-    import numpy as np    
-    #from model import model
+import numpy as np
+import matplotlib.pyplot as plt
+from pysb.integrate import Solver
 
+def run_model(model, save_plot='model_plot.png'):
     t = np.linspace(0, 10*3600, 10*60)
 
     model.parameters['VEMURAFENIB_0'].value = 0
@@ -12,7 +11,7 @@ def run_model(model, save_plot='model_plot.png'):
     solver.run()
     y1 = solver.y
     yobs1 = solver.yobs
-    # new initial conditions for simulation post event
+    # New initial conditions for simulation post event
     y_init = solver.y[-1]
     # Change level of Vemurafenib from 0 to 2e4 molecules/cell
     assert len(model.observables['Vem_obs'].species) == 1, \
@@ -20,18 +19,16 @@ def run_model(model, save_plot='model_plot.png'):
 
     y_init[model.observables['Vem_obs'].species[0]] = 2e5
 
-    # continue model simulation with y_init as new initial condition
+    # Continue model simulation with y_init as new initial condition
     solver = Solver(model, t)
     solver.run(y0=y_init)
     y2 = solver.y
     yobs2 = solver.yobs
 
-    # Append 
+    # Concatenate the two simulations
     yout = np.concatenate((y1[:-1], y2), axis=0)
     yobs = np.concatenate((yobs1[:-1], yobs2), axis=0)
     tout = np.append(t[:-1], t + t[-1])
-
-    import matplotlib.pyplot as plt
 
     plt.plot(t, yobs2['ERK_p']/yobs2['ERK_p'][0],
              t, yobs2['RAS_active']/yobs2['RAS_active'][0], linewidth=5)
