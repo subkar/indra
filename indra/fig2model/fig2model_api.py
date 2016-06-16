52e9779544c8650b2c77b972059a11c19ae67699
subkar <sub.kartik@gmail.com>
2016-06-16 07:01:45 -0400

    api refactored, primary ouput of query is figure panels

--- indra/fig2model/fig2model_api.py
+++ indra/fig2model/fig2model_api.py
@@ -1,19 +1,32 @@
 import sourcedata_client as sdc
 from process import Publication, Hypothesis
+from process2 import Panel
 
-def process_query(query, mlist=[], save_json_name = 'sd_output.json'):
+def process_query(query, save_json_name = 'sd_output.json'):
     dict = sdc.get_json(query, limit=100)
     sdc.save_json(dict, save_json_name)
-    return process_json(dict, mlist)
+    return process_ids(get_panelids(dict))
 
 
-def process_json(dict, mlist):
-    papers = []
+def get_panelids(dict):
+    panel_ids = []
     for paper in dict['results']['direct']:
-        if paper['pmid'] != None:
-            p = Publication(paper)
-            p.hypotheses(paper, mlist)
-            papers.append(p)            
-    return papers
+        for panel in paper['hypos'][0]['panels']:
+            panel_ids.append(panel['panel_id'])
+
+    return panel_ids
 
 
+def process_ids(panel_ids):
+    assert type(panel_ids) == list
+    panels = []
+    for id in panel_ids:
+        panel_dict = sdc.get_panel_info(id)
+        pmid = sdc.get_pmids(panel_dict)
+        p = Panel(panel_dict, pmid)
+        p.figure_label(panel_dict)
+        p.caption(panel_dict)
+        p.href(panel_dict)
+        panels.append(p)
+
+    return panels
