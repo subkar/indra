767469b49d92ebc3c15c87b1ce970b809003e5f9
subkar <sub.kartik@gmail.com>
2016-06-16 07:02:36 -0400

    json results are extracted as panel classes

---
+++ indra/fig2model/process2.py
@@ -0,0 +1,34 @@
+class Panel(object):
+    def __init__(self, panel, pmid):
+        self.pmid = pmid
+
+
+    def figure_label(self, panel):
+        for p in panel['figure']['panels']:
+            if p['panel_id'] == panel['current_panel_id']:
+                self.figure_label =  p['label']            
+
+                
+    def __str__(self):
+        return ("Publication PMID:" + self.pmid)
+
+    
+    def __repr__(self):
+        return ("<%s in  PMID%s at 0x%x>" %
+                (self.figure_label, self.pmid, id(self)))
+
+
+    def caption(self, panel):
+        for p in panel['figure']['panels']:
+            if p['panel_id'] == panel['current_panel_id']:
+                self.caption = p['formatted_caption']
+
+
+    def href(self, panel):
+        for p in panel['figure']['panels']:
+            if p['panel_id'] == panel['current_panel_id']:
+                self.href = p['href']           
+
+
+
+
